229f8f90cf7b7ba0537f47a4b1d7cf1d6bc2b0f0
Mika Munterud <mika.munterud@outlook.com>
2023-05-16 16:02:09 +0200

    feat: adding initial calc route

---
+++ routes/calc.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const { Calculator } = require('../calculator');
+
+const router = express.Router();
+
+/* GET calculator. */
+router.get('/calc', (req, res) => {
+  const { val } = req.query;
+  const calc = new Calculator();
+
+  res.render('index', { title: 'Express' });
+});
+
+module.exports = router;
