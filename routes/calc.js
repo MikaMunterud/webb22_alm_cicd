4825ef02687d2a2183a53092db0c4d99af77cf55
Mika Munterud <mika.munterud@outlook.com>
2023-05-16 23:57:59 +0200

    feat: adding calc routes for each calculator class function

--- routes/calc.js
+++ routes/calc.js
@@ -1,14 +1,52 @@
+/* eslint-disable radix */
 const express = require('express');
-// const { Calculator } = require('../calculator');
 
 const router = express.Router();
+const { Calculator } = require('../public/javascripts/calculator');
 
+const calculator = new Calculator();
 /* GET calculator. */
-router.get('/calc', (req, res) => {
-  //   const { val } = req.query;
-  //   const calc = new Calculator();
+router.get('/', (req, res) => {
+  res.render('calc', { result: calculator.getResult() });
+});
 
-  res.render('index', { title: 'Express' });
+router.post('/add', (req, res) => {
+  const { value } = req.body;
+  calculator.add(parseInt(value));
+  res.redirect('/calc');
+});
+
+router.post('/subtract', (req, res) => {
+  const { value } = req.body;
+  calculator.subtract(parseInt(value));
+  res.redirect('/calc');
+});
+
+router.post('/multiply', (req, res) => {
+  const { value } = req.body;
+  calculator.multiply(parseInt(value));
+  res.redirect('/calc');
+});
+
+router.post('/divide', (req, res) => {
+  const { value } = req.body;
+  calculator.divide(parseInt(value));
+  res.redirect('/calc');
+});
+
+router.post('/clear', (req, res) => {
+  calculator.clear();
+  res.redirect('/calc');
+});
+
+router.post('/clearAll', (req, res) => {
+  calculator.clearAll();
+  res.redirect('/calc');
+});
+
+router.post('/undo', (req, res) => {
+  calculator.undo();
+  res.redirect('/calc');
 });
 
 module.exports = router;
