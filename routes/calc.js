9daf944f67624a3ff1cbf6a588ebb7315012d986
Mika Munterud <mika.munterud@outlook.com>
2023-05-16 16:12:03 +0200

    fix: commenting out unused variables temporarily

--- routes/calc.js
+++ routes/calc.js
@@ -1,12 +1,12 @@
 const express = require('express');
-const { Calculator } = require('../calculator');
+// const { Calculator } = require('../calculator');
 
 const router = express.Router();
 
 /* GET calculator. */
 router.get('/calc', (req, res) => {
-  const { val } = req.query;
-  const calc = new Calculator();
+  //   const { val } = req.query;
+  //   const calc = new Calculator();
 
   res.render('index', { title: 'Express' });
 });
