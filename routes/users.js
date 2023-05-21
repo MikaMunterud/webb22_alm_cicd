96e1f136fece6a73011ed9e9ad7fe807f073ec98
Mika Munterud <mika.munterud@outlook.com>
2023-05-21 23:30:31 +0200

    feat: updating name oder to be in the same order as group name

--- routes/users.js
+++ routes/users.js
@@ -3,9 +3,9 @@ const express = require('express');
 const router = express.Router();
 
 const users = [
-  { username: 'MM', firstName: 'Mika', surname: 'Munterud' },
   { username: 'AW', firstName: 'Alexander', surname: 'Wiklöv' },
   { username: 'LH', firstName: 'Linus', surname: 'Härén' },
+  { username: 'MM', firstName: 'Mika', surname: 'Munterud' },
 ];
 
 /* GET users listing. */
